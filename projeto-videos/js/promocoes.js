931ebb6544945a8660fab60a612d1aa371840146
KaykeAmorim <kaykeamorim6@gmail.com>
2022-05-11 08:40:41 -0300

    Atualizaçãao promoções 11/05/2022

--- projeto-videos/js/promocoes.js
+++ projeto-videos/js/promocoes.js
@@ -1,22 +1,53 @@
-window.onload=function(){
+var galeria = document.querySelector("#tabelaPromocoes")
+var textoLorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est suscipit ipsa vel in laborum delectus nihil officiis optio repudiandae aliquam?"
 
-    var livros=[
-        ["Mick","mick.jpg","Mick Jagger não exagera ao afirmar que se escrevesse sobre sua vida “em todos os detalhes, as pessoas ficariam aterrorizadas”. Nesta biografia do líder dos Rolling Stones, o jornalista americano Christopher Andersen não poupa fãs e leitores dos detalhes polêmicos e nada glamourosos da trajetória artística e pessoal de um dos astros mais originais do rock contemporâneo."],
-        ["Quem somos nós","quem-somos-nos.jpg","De que são feitos o pensamento e a realidade? E como um pensamento muda a natureza da realidade? Este livro conduz por meio da ciência para dentro de um universo que é mais vivo do que se pode imaginar - a fronteira final do conhecimento científico sobre a consciência a percepção a química do corpo e a estrutura cerebral. Uma obra sobre física quântica espiritualidade e o significado da vida."],
-        ["A cura quântica","cura-quantica.jpg","Há uma abordagem da medicina na qual a mente, a consciência e a inteligência ocupam papéis principais. Segundo essa vertente, as doenças ocorrem quando nosso corpo vital está ferido."],
-        ["O pequeno Príncipe","pequeno-principe.jpg","Nesta clássica história que marcou gerações de leitores em todo o mundo, um piloto cai com seu avião no deserto do Saara e encontra um pequeno príncipe, que o leva a uma jornada filosófica e poética através de planetas que encerram a solidão humana. A edição conta com a clássica tradução do poeta imortal dom Marcos Barbosa, e é a versão mais consagrada da obra, publicada no Brasil desde 1952."]
-    ]
+var videos = [
+    ["Video 1", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 2", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 3", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 4", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 5", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 6", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 7", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 8", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+    ["Video 9", "https://capas-p.imagemfilmes.com.br/164908_000_p.jpg"],
+]
 
-    console.log(livros)
+for (let i = 0; i < videos.length; i++){
+    
+    var divVideo = configuraElemento(criarDiv(), "col-sm-4 mt-4", galeria)
+    var tituloVideo = configuraElemento(criarTexto(videos[i][0], "h5"), "", divVideo)
+    var divDetalhes = configuraElemento(criarDiv(), "row", divVideo)
+    var divImagem = configuraElemento(criarDiv(), "col-sm-4", divDetalhes)
+    var imagem = configuraElemento(criarImg(videos[i][1]), "w-100", divImagem)
+    var divSinopse = configuraElemento(criarDiv(), "col-sm-8", divDetalhes);
+    var sinopse = configuraElemento(criarTexto(textoLorem,"p"), "text-dark", divSinopse)
+}
 
-    var galeria=document.querySelector("#galeria")
 
-    for(var i=0; i<livros.length; i++){
-        var img = new Image()
-        img.src="../imagens/" + livros[i][1]
-        img.width=120
-        galeria.innerHTML+= "<p>" + livros[i][0] + "</p>"
-        galeria.appendChild(img) //Anexa a imagem na galeria
-        galeria.innerHTML+= "<p>" + livros[i][2] + "</p>"
-    }
+function criarDiv(){
+    var element = document.createElement("div")
+    return element;
+}
+
+function criarTexto(valor, tipo){
+    var element = document.createElement(tipo)
+    element.innerHTML=valor;
+    return element;
+}
+
+function criarImg(caminho){
+    var element = document.createElement("img")
+    element.setAttribute("src", caminho)
+    return element;
+}
+
+function configuraElemento(element, config, destino){
+    element.setAttribute("class",config)
+    colocarDentro(destino, element)
+    return element;
+}
+
+function colocarDentro(destino, element){
+    destino.appendChild(element)
 }
