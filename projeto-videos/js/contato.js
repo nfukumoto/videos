68b75787bd59a96b3bf098e3436c864d52fc7085
rpereira <renanpereira@id.uff.br>
2022-05-18 15:23:39 -0300

    Páginas de contato atualizadas

--- projeto-videos/js/contato.js
+++ projeto-videos/js/contato.js
@@ -9,8 +9,22 @@ window.onload=function(){
             alert("Preecha o assunto!")            
         }else if(formC.comentario.value == ""){
             alert("Preencha o comentario!")
-        }else{ 
-            formC.submit()
+        }else if(formC.chamado.checked == false){
+            alert('Marque a confirmação!')
+        }else{
+            alert('Seu chamado foi aberto com sucesso!')
+            addChamado()
         }
+    }
+    
+
+    function addChamado(){
+        let id=parseInt(Math.random()*90000 + 10000)
+        let valor=new Array 
+            valor.push(formC.nomeContato.value)
+            valor.push(formC.emailContato.value)
+            valor.push(formC.assuntoContato.value)
+            valor.push(formC.comentario.value)
+        localStorage.setItem('chamado-'+id,JSON.stringify(valor))
     }
 }
