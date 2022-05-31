b6abfb4e1ab6848322d387f16ea971c6006aa213
KaykeAmorim <kaykeamorim6@gmail.com>
2022-05-31 15:57:15 -0300

    Mudanças 31.05.22

---
+++ projeto-videos/node/leTxt.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const http = require('http');
+
+http.createServer((req,res) => {
+    fs.appendFile("log-users.txt","testando 1,2,3...\n",(err) => {
+        if(err) throw err
+
+        res.write("Escrevi");
+        res.end();
+    });
+}).listen(8080)
