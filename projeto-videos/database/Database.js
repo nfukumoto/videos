eae244bf977f01d6f18162b04680c29b7e5bd949
rpereira <renanpereira@id.uff.br>
2022-06-20 16:28:35 -0300

    Início da implementação da sessão. Falta terminar

--- projeto-videos/database/Database.js
+++ projeto-videos/database/Database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const connection = new Sequelize('projeto_video','nfukumoto','24052003nN@!',{
+const connection = new Sequelize('projeto_video','renanp','Ren@nm73626',{
     host:'localhost',
     dialect: 'mysql',
     timezone:"-03:00"
